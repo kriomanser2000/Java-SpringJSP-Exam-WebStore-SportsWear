75004a416a99b6b0b55a060da601666df6aee85a
kriomanser2000 <kuzmenkasana05@gmail.com>
2024-12-06 16:17:09 +0200

    Initial commit

---
+++ src/main/java/com/example/javaexamwebstoresportswear/JavaExamWebStoreSportsWearApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaexamwebstoresportswear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExamWebStoreSportsWearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaExamWebStoreSportsWearApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaexamwebstoresportswear/JavaExamWebStoreSportsWearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaexamwebstoresportswear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExamWebStoreSportsWearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
